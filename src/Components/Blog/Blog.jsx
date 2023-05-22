f2f42dd37d47dbb03e178cc01d842f2b6057a69f
siddarthabiswas <sidubiswas6@gmail.com>
2023-05-22 21:43:06 +0600

    added third ans

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -29,15 +29,9 @@ const Blog = () => {
                     </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey="3">
-                    <Accordion.Header>Accordion Item #2</Accordion.Header>
+                    <Accordion.Header>	What is MongoDB aggregate and how does it work ?</Accordion.Header>
                     <Accordion.Body>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                        aliquip ex ea commodo consequat. Duis aute irure dolor in
-                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                        culpa qui officia deserunt mollit anim id est laborum.
+                    What is Aggregation in MongoDB? Aggregation is a way of processing a large number of documents in a collection by means of passing them through different stages. The stages make up what is known as a pipeline. The stages in a pipeline can filter, sort, group, reshape and modify documents that pass through the pipeline.
                     </Accordion.Body>
                 </Accordion.Item>
             </Accordion>
